4034aae3bc7db7626ebdb70072eac2ad419a66be
Jinfort <mr.jinfortalexey@gmail.com>
2023-12-04 13:17:10 +0500

    Мороз: добавлен эквайринг

--- kitreport/kitreport/settings.py
+++ kitreport/kitreport/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'kitreport.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'kitreport', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
