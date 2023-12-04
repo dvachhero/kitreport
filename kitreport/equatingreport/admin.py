2b2ffbc455e3c877f53cf537186c8d131fc4b303
Jinfort <mr.jinfortalexey@gmail.com>
2023-12-04 13:11:04 +0500

    Мороз: добавлен эквайринг

--- kitreport/equatingreport/admin.py
+++ kitreport/equatingreport/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, InventoryReport500Handbook, InventoryReport500
+from .models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['username', 'full_name', 'department']
