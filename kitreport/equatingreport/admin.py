f6aa458fb8c49c5267c1fab260e472d322eea597
Jinfort <mr.jinfortalexey@gmail.com>
2023-12-04 12:52:05 +0500

    Мороз: добавлен эквайринг

--- kitreport/equatingreport/admin.py
+++ kitreport/equatingreport/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserProfile, sprforekv, sprforfn
+from .models import UserProfile, InventoryReport500Handbook, InventoryReport500
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['username', 'full_name', 'department']
@@ -7,24 +7,12 @@ class UserProfileAdmin(admin.ModelAdmin):
 
 admin.site.register(UserProfile, UserProfileAdmin)
 
+class InventoryReport500HandbookAdmin(admin.ModelAdmin):
+    list_display = ('BE', 'Financial', 'DocumentNumber', 'DateProcessed', 'Processed')
 
-class SprForEkvAdmin(admin.ModelAdmin):
-    # Эти поля будут отображаться в списке
-    list_display = ('TID', 'name_rp', 'pfm', 'street', 'fio_rp', 'id_rp')
+class InventoryReport500Admin(admin.ModelAdmin):
+    list_display = ('BE', 'Financial', 'DocumentNumber', 'DateProcessed', 'Processed')
 
-    # Эти поля будут использоваться для фильтрации в списке
-    list_filter = ('pfm', 'street', 'fio_rp')
-
-    # Поля, по которым будет осуществляться поиск
-    search_fields = ('name_rp', 'street', 'comment', 'fio_rp')
-admin.site.register(sprforekv, SprForEkvAdmin)
-
-class SprForFnAdmin(admin.ModelAdmin):
-    # Эти поля будут отображаться в списке
-    list_display = ('code_fn', 'name_rp', 'pfm')
-
-    # Поля, по которым будет осуществляться поиск
-    search_fields = ('code_fn', 'name_rp', 'pfm')
-admin.site.register(sprforfn, SprForFnAdmin)
-
+admin.site.register(InventoryReport500Handbook, InventoryReport500HandbookAdmin)
+admin.site.register(InventoryReport500, InventoryReport500Admin)
 
