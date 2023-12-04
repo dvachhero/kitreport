9944b01ae1ffb8454bcc0867f7a696feb1330790
ZybinSK <dvachhero@yandex.ru>
2023-12-04 16:08:27 +0500

    Добавил шаблоны и внес изменения в setting.py

--- kitreport/inventoryreport/views.py
+++ kitreport/inventoryreport/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
 
-# Create your views here.
+@login_required(login_url='/login/')
+def inventory_report_upload(request):
+    # Ваша логика обработки запроса, если необходимо
+    return render(request, 'inventoryreportupload.html')
--- kitreport/kitreport/urls.py
+++ kitreport/kitreport/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from equatingreport.views import equating_report_upload, login_view, homefd
+from inventoryreport.views import inventory_report_upload
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('equatingreportupload/', equating_report_upload, name='equating_report_upload'),
     path('login/', login_view, name='login'),
     path('homefd/', homefd, name='homefd'),
+    path('inventoryreportupload/', inventory_report_upload, name='inventory_report_upload'),
+
 
 ]
