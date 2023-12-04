54b9aa3d6eecbf863e9aff6ebc21590efd9c96a2
Jinfort <mr.jinfortalexey@gmail.com>
2023-12-04 12:52:05 +0500

    Мороз: добавлен эквайринг

--- kitreport/equatingreport/admin.py
+++ kitreport/equatingreport/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import UserProfile, InventoryReport500Handbook, InventoryReport500
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['username', 'full_name', 'department']
+    search_fields = ['user__username','full_name', 'department']
+
+admin.site.register(UserProfile, UserProfileAdmin)
+
+class InventoryReport500HandbookAdmin(admin.ModelAdmin):
+    list_display = ('BE', 'Financial', 'DocumentNumber', 'DateProcessed', 'Processed')
+
+class InventoryReport500Admin(admin.ModelAdmin):
+    list_display = ('BE', 'Financial', 'DocumentNumber', 'DateProcessed', 'Processed')
+
+admin.site.register(InventoryReport500Handbook, InventoryReport500HandbookAdmin)
+admin.site.register(InventoryReport500, InventoryReport500Admin)
+
