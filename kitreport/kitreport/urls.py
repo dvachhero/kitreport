25fa819ed7dc3220fc622054ddb5c426b2d48d46
pael <fedorov2777dmitry@gmail.com>
2023-12-18 13:29:31 +0500

    modified:   .gitignore 	deleted:    kitreport/.~lock.mismatched_data.xlsx# 	deleted:    kitreport/combined_ekv.xlsx 	deleted:    kitreport/inventoryreport/migrations/0001_initial.py 	deleted:    kitreport/inventoryreport/migrations/0002_rename_yourmodelname_inventoryreportdf_and_more.py 	deleted:    kitreport/inventoryreport/migrations/0003_inventoryreporterrors.py 	deleted:    kitreport/inventoryreport/migrations/0004_forreport_inventoryreporterror_and_more.py 	deleted:    kitreport/inventoryreport/migrations/0005_remove_forreport_code_fm_forreport_code_fn_and_more.py 	deleted:    kitreport/inventoryreport/migrations/0006_alter_forreport_code_fn.py 	deleted:    kitreport/inventoryreport/migrations/0007_alter_forreport_code_fn.py 	modified:   kitreport/kitreport/urls.py 	deleted:    kitreport/mismatched_data.xlsx

--- kitreport/kitreport/urls.py
+++ kitreport/kitreport/urls.py
@@ -1,16 +1,21 @@
 from django.contrib import admin
 from django.urls import path
 from equatingreport.views import equating_report_upload, login_view, homefd
-from inventoryreport.views import inventory_report_upload
+from inventoryreport.views import inventory_report_upload, download_file, upload_success
 from equatingreport.views import upload_files, check_fn
 
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('equatingreportupload/', upload_files, name='equating_report_upload'),
+    path('', homefd, name='homefd'),
+    path('equatingreportupload/', equating_report_upload, name='equating_report_upload'),
     path('login/', login_view, name='login'),
     path('homefd/', homefd, name='homefd'),
     path('inventoryreportupload/', inventory_report_upload, name='inventory_report_upload'),
+    path('inventoryreportupload/success/', upload_success, name='upload_success'),
+    path('inventoryreportupload/success/download-file/', download_file, name='download_file'),
     path('check_fn/', check_fn, name='check_fn'),
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
