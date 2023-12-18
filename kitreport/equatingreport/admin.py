1c0249d3fee1fea0a2855f6744a5517b77217e76
dvachhero <143478488+dvachhero@users.noreply.github.com>
2023-12-18 12:58:48 +0500

    Update admin.py

--- kitreport/equatingreport/admin.py
+++ kitreport/equatingreport/admin.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
-from .models import UserProfile, InventoryReport500Handbook, InventoryReport500
+from .models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ['username', 'full_name', 'department']
     search_fields = ['user__username','full_name', 'department']
 
 admin.site.register(UserProfile, UserProfileAdmin)
-
-class InventoryReport500HandbookAdmin(admin.ModelAdmin):
-    list_display = ('BE', 'Financial', 'DocumentNumber', 'DateProcessed', 'Processed')
-
-class InventoryReport500Admin(admin.ModelAdmin):
-    list_display = ('BE', 'Financial', 'DocumentNumber', 'DateProcessed', 'Processed')
-
-admin.site.register(InventoryReport500Handbook, InventoryReport500HandbookAdmin)
-admin.site.register(InventoryReport500, InventoryReport500Admin)
 
